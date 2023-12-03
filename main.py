cbf1b5dd7aaf0eed1445c1e88abb6a4977aa7654
Richie Goulazian <rgoulazian@gmail.com>
2023-12-03 00:12:36 -0800

    Complete a basic transformer model

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ CHUNK_SIZE = 64
 BATCH_SIZE = 32
 EVAL_ITERATIONS = 100
 EMBEDDING_SIZE = 32
-HEAD_SIZE = 16
+NUM_HEADS = 4
+HEAD_SIZE = EMBEDDING_SIZE // NUM_HEADS
+
+assert EMBEDDING_SIZE % NUM_HEADS == 0
 
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda")
@@ -23,8 +26,12 @@ class FeedForward(nn.Module):
     def __init__(self, embedding_size: int):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(in_features=embedding_size, out_features=embedding_size),
+            # The feedforward layer is 4x the size of the embedding dimension.
+            # This is following the practice in the transformer paper.
+            nn.Linear(in_features=embedding_size, out_features=4 * embedding_size),
             nn.ReLU(),
+            # Transform back into the residual pathway.
+            nn.Linear(in_features=4 * embedding_size, out_features=embedding_size),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -32,7 +39,7 @@ class FeedForward(nn.Module):
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, head_size: int = HEAD_SIZE):
+    def __init__(self, head_size: int):
         super().__init__()
 
         # Bias is False, yielding a simple matrix multiplication with learnable weights
@@ -67,10 +74,30 @@ class MultiHeadedAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([SelfAttention(head_size=head_size) for _ in range(num_heads)])
 
+        output_size = head_size * num_heads
+        self.projection = nn.Linear(in_features=output_size, out_features=output_size)
+
     def forward(self, x: torch.Tensor):
         # Multi-headed attention is simply using multiple self-attention heads and concatenating
         # the outputs together.
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        x = torch.cat([head(x) for head in self.heads], dim=-1)
+        
+        # Use a linear transformation to "recombine" the multi-headed attention
+        # and transform back into the result for the residual pathway.
+        return self.projection(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.msa_head = MultiHeadedAttention(num_heads=NUM_HEADS, head_size=HEAD_SIZE)
+        self.feedforward = FeedForward(embedding_size=EMBEDDING_SIZE)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # The addition of x is a residual connection to improve gradient propagation.
+        x = x + self.msa_head(x)
+        x = x + self.feedforward(x)
+        return x
 
 
 class BigramLanguageModel(nn.Module):
@@ -90,8 +117,11 @@ class BigramLanguageModel(nn.Module):
             embedding_dim=EMBEDDING_SIZE,
         )
 
-        # For now, the self-attention head size is the same as the embedding size.
-        self.msa_head = MultiHeadedAttention(num_heads=4, head_size=EMBEDDING_SIZE // 4)
+        self.transformer_blocks = nn.Sequential(
+            TransformerBlock(),
+            TransformerBlock(),
+            TransformerBlock(),
+        )
 
         self.feedforward = FeedForward(embedding_size=EMBEDDING_SIZE)
 
@@ -107,10 +137,9 @@ class BigramLanguageModel(nn.Module):
 
         token_embeddings = self.token_embedding_table(idx) # [batch_size, sequence_length, embedding_size]
         positional_embeddings = self.position_embedding_table(torch.arange(sequence, device=DEVICE)) # [sequence_length, embedding_size]
-        x = token_embeddings + positional_embeddings # [batch_size, sequence_length, embedding_size]
-        x = self.msa_head(x)
-        x = self.feedforward(x)
         
+        x = token_embeddings + positional_embeddings # [batch_size, sequence_length, embedding_size]
+        x = self.transformer_blocks(x)
         logits = self.language_model_head(x) # [batch_size, sequence_length, vocab_size]
 
         if targets is None:
@@ -200,7 +229,10 @@ def main():
     bigram_model = BigramLanguageModel(vocab_size=len(vocabulary)).to(DEVICE)
 
     optimizer = torch.optim.AdamW(bigram_model.parameters(), lr=1e-3)
-    for _ in tqdm(range(10_000), desc="Training"):
+    for i in tqdm(range(3_000), desc="Training"):
+        if i % 1000 == 0:
+            print(f"Current loss: {estimate_loss(model=bigram_model, train=train_set, test=test_set)}")
+
         xb, yb = get_batch(dataset=train_set)
 
         _logits, loss = bigram_model(xb, yb)
@@ -212,7 +244,8 @@ def main():
     print(f"Final loss: {estimate_loss(model=bigram_model, train=train_set, test=test_set)}")
 
     print("Test generation:")
-    print(''.join(decode(bigram_model.generate(torch.zeros((1, 1), dtype=torch.long, device=DEVICE), max_new_tokens=1000)[0].tolist())))
+    while True:
+        print(''.join(decode(bigram_model.generate(torch.zeros((1, 1), dtype=torch.long, device=DEVICE), max_new_tokens=1000)[0].tolist())))
 
 
 if __name__ == "__main__":
